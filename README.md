def5e04edd344da278f03f98fb5bf15e9c697465
Adam <adam.m.thompson@icloud.com>
2019-11-17 18:50:34 -0500

    api updates

--- README.md
+++ README.md
@@ -1,5 +1,34 @@
+# Getting started 
+
 ## Backend
-To run the backend in dev mode, go to `cd ./api` and run `npm run watch`
+To run the backend, first you'll need an API key from [The Movie Database](https://developers.themoviedb.org/3/getting-started). Once you have your v3 key, add it to a `.env` file in the `/api/` folder as `TMDB_API_KEY={my_api_key}`.
+Once you've done that run the commands
+```
+cd ./api
+npm run watch
+```
+to run the backend in dev mode (or `npm run start` to just start it)
 
 ## Frontend
 To run the frontend in dev mode, go to `cd ./client` and run `yarn start`
+
+# TODO
+1. Add filtering by genre
+
+I'd like to add filtering by genre. To do this, I would need to add a genres nav bar in the header. This would be populated by a request to the TMDb api endpoint: `/genre/movie/list`. Then I'd add a param to the `/popular` endpoint that is the requested genre. On the backend I'd filter for movies of that genre and return that array. (Would likely need to get multiple "pages" of popular movies)
+
+~~2. Show related movies~~
+
+Currently done by appending related movies to the data returned from the api. This could also be done by hitting a separate endpoint, but this involves more `http` requests from the client (could have performance implications)
+
+3. Add a page for individual actor details
+
+Similar to how the movies page works, an actor details page would look at the url params to populate an actor's profile
+
+4. Add a caching layer for your requests to the 3rd party API.
+
+By caching data in the backend, we can save on calls to the TMDb api. We can also cache data on the frontend to save on `http` requests on from the client
+
+5. Add unit testing for your API.
+
+I should test that each api endpoint returns the expected data, or returns an appropriate status code
--- api/routes/movie.js
+++ api/routes/movie.js
@@ -2,6 +2,7 @@ const express = require('express');
 const https = require('https');
 const router = express.Router();
 const dotenv = require('dotenv');
+const axios = require('axios');
 dotenv.config();
 
 // All routes here are prefised with movie/
@@ -13,66 +14,60 @@ router.get('/', (req, res) => {
 
 router.get('/popular', (req, res) => {
   const requestURL = composeTMDbURL('movie/popular')
-
+  
   // Hit the TMDb api
-  https.get(requestURL, tmbdResponse => {
-    
-    // If something goes wrong with the request, return a 400
-    if (isError(tmbdResponse)) return tmbdResponse.status(tmbdResponse.statusCode)
-
-    let data = ''
-    tmbdResponse.on('data', chunk => {
-      data += chunk
-    })
-    // Return the results as json
-    tmbdResponse.on('end', () => {
-      const json = JSON.parse(data)
-      if (json.results.length === 0) {
+  axios.get(requestURL)
+    .then(tmbdResponse => {
+      data = tmbdResponse.data
+      if (data.results.length === 0) {
         res.status(400)
       } else {
-        res.json(json.results)
+        res.json(data.results)
       }
     })
-  })
+    .catch(err => {
+      console.log(err)
+      res.sendStatus(400);
+    })
+
   // res.send(`You're gonna be popular! I'll teach you the proper poise, When you talk to boys, Little ways to flirt and flounce,`)
 })
 
-// Get the queried movie
-// test with: 'Harry Poter'
-// test with: 'qwertyuiop'
+// Get the queried movie list
 router.get('/search/:query', (req, res) => {
   const requestURL = composeTMDbURL(`search/movie`, ['language=en-US', `query=${req.params.query}`, 'page=1', 'include_adult=false'])
-  https.get(requestURL, tmbdResponse => {
-    if (isError(tmbdResponse)) return res.status(tmbdResponse.statusCode)
-    let data = ''
-    tmbdResponse.on('data', chunk => {
-      data += chunk
-    })
-    tmbdResponse.on('end', () => {
-      const json = JSON.parse(data)
-      if(json.results.length === 0) {
-        res.status(400)
-      } else {
-        res.json(json.results)
-      }
-    })
+
+  axios.get(requestURL).then(tmdbResp => {
+    res.json(tmdbResp.data.results)
+  })
+  .catch(err => {
+    res.sendStatus(400)
   })
 })
 
-// test with: 671 // Harry Potter and the Philosopher's Stone
-// test with 1 // 400 response
+// Since the default movie response doesn't give the cast or related movies
+// we need to send multiple requests to the API.
+
+// Ideally I'd add more params to the url, or different targets but this is good enough for a POC.
+
 router.get('/:movieId', (req, res) => {
   const requestURL = composeTMDbURL(`movie/${req.params.movieId}`, ['language=en-US', 'include_adult=false'])
-  https.get(requestURL, tmbdResponse => {
-    if (isError(tmbdResponse)) return res.status(tmbdResponse.statusCode)
-    let data = ''
-    tmbdResponse.on('data', chunk => {
-      data += chunk
-    })
-    tmbdResponse.on('end', () => {
-      const json = JSON.parse(data)
-      res.json(json)
-    })
+  const castURL = composeTMDbURL(`movie/${req.params.movieId}/credits`, ['language=en-US'])
+  const relatedURL = composeTMDbURL(`movie/${req.params.movieId}/similar`, ['language=en-US'])
+
+  const moviesRequest = axios.get(requestURL)
+  const castRequest = axios.get(castURL);
+  const relatedRequest = axios.get(relatedURL);
+
+  Promise.all([moviesRequest, castRequest, relatedRequest]).then(tmdbResp => {
+    const movie = tmdbResp[0].data
+    const cast = tmdbResp[1].data
+    movie.cast = cast.cast
+    movie.related = tmdbResp[2].data.results
+    res.json(movie)
+  })
+  .catch(err => {
+    res.sendStatus(400)
   })
 })
 
